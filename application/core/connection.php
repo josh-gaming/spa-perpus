415b5f510c25229bfbbac8d4d5664b48c9137e98
josh-gaming <gamingjosh512@gmail.com>
2021-10-01 19:40:27 +0700

    add meta

--- application/core/connection.php
+++ application/core/connection.php
@@ -1,10 +1,9 @@
 <?php
 
-define("HOST","localhost");
-define("USER","root");
-define("PASSWORD","");
-define("DATABASE","dbs_perpus");
+#edit this form to your database
+define("HOST", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DATABASE", "dbs_perpus");
 
-$connect = new mysqli(HOST,USER,PASSWORD,DATABASE);
-
-?>
+$connect = new mysqli(HOST, USER, PASSWORD, DATABASE);
